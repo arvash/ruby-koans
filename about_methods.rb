3ecce7905c059ee7e23e0c988e804e5d0ddab38d
arvash <arvash@milaap.org>
2012-01-16 11:57:03 +0530

    done till control statements

--- about_methods.rb
+++ about_methods.rb
@@ -113,14 +113,14 @@ class AboutMethods < EdgeCase::Koan
   private :my_private_method
 
   def test_calling_private_methods_without_receiver
-    assert_equal __, my_private_method
+    assert_equal "a secret", my_private_method
   end
 
   def test_calling_private_methods_with_an_explicit_receiver
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       self.my_private_method
     end
-    assert_match /__/, exception.message
+    assert_match(/^private method \`\w+\' called for/, exception.message)
   end
 
   # ------------------------------------------------------------------
@@ -139,12 +139,12 @@ class AboutMethods < EdgeCase::Koan
 
   def test_calling_methods_in_other_objects_require_explicit_receiver
     rover = Dog.new
-    assert_equal __, rover.name
+    assert_equal "Fido", rover.name
   end
 
   def test_calling_private_methods_in_other_objects
     rover = Dog.new
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       rover.tail
     end
   end
